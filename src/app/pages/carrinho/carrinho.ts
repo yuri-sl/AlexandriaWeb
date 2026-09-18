5980432b1c52375b0aedd93b323be5257b60f773
Yuri Santana <yuri03ysl@gmail.com>
2026-09-18 18:02:02 -0300

    Atualizações na página de carrinho

--- src/app/pages/carrinho/carrinho.ts
+++ src/app/pages/carrinho/carrinho.ts
@@ -1,6 +1,14 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../../shared/navbar/navbar';
 
+
+type Obra = {
+  titulo:string,
+  preco:bigint,
+  quantidade:number,
+  autor:string,
+  marcador:string
+}
 @Component({
   selector: 'app-carrinho',
   imports: [Navbar],
@@ -8,5 +16,34 @@ import { Navbar } from '../../shared/navbar/navbar';
   styleUrl: './carrinho.scss',
 })
 export class Carrinho {
+    totalCompra:number = 0;
+
+    tipoEntrega: string = '';
+    tipoPagamento: string = '';
+
+
+    obra1:Obra = ({
+      titulo: "as",
+      preco: BigInt(12.00),
+      quantidade:2,
+      autor:"Peter",
+      marcador:"história"
+    })
+
+    obra2:Obra = ({
+      titulo: "as",
+      preco: BigInt(12.00),
+      quantidade:2,
+      autor:"Peter",
+      marcador:"história"
+    })
+    listaCarrinho:Obra[] = [this.obra1,this.obra2];
+
+    atualizarValor(Obra1:Obra){
+      return BigInt(this.obra1.quantidade) * this.obra1.preco;
+
+    }
+
+
 
 }
